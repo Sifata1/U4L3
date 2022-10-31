02d7bc156a03fdf09cabe5fc62fc3e953f1ee9f9
sifatanan <sanan6576@bths.edu>
2022-10-31 18:00:58 -0400

    finished sum of digits

--- src/WhileLoopFun.java
+++ src/WhileLoopFun.java
@@ -1,25 +1,26 @@
-public class WhileLoopFun
-{
-    /**This class has no instance variables, so we can either include no constructor,
-     or this simple no-parameter constructor, which is actually preferred practice
-     over omitting the constructor entirely
+public class WhileLoopFun {
+    /**
+     * This class has no instance variables, so we can either include no constructor,
+     * or this simple no-parameter constructor, which is actually preferred practice
+     * over omitting the constructor entirely
      */
-    public WhileLoopFun() { }
+    public WhileLoopFun() {
+    }
 
-    /**Performs the factorial operation on number (i.e. number!) and returns the result as a double
-     (returning a double rather than an int is intentional, as it allows for much larger results to be returned
-     than would be possible if returning an int, because with factorials, it is easy to quickly exceed
-     Integer.MAX_VALUE and get unexpected results).
-     Example:  If number is 6, this methods returns 6 * 5 * 4 * 3 * 2 * 1 = 720.0
-     Example:  If number is 10, this methods returns 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1 = 362880.0
-     Example:  If number is 20, this methods returns 2.43290200817664E18 ≈ 2.43 x 1018
+    /**
+     * Performs the factorial operation on number (i.e. number!) and returns the result as a double
+     * (returning a double rather than an int is intentional, as it allows for much larger results to be returned
+     * than would be possible if returning an int, because with factorials, it is easy to quickly exceed
+     * Integer.MAX_VALUE and get unexpected results).
+     * Example:  If number is 6, this methods returns 6 * 5 * 4 * 3 * 2 * 1 = 720.0
+     * Example:  If number is 10, this methods returns 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1 = 362880.0
+     * Example:  If number is 20, this methods returns 2.43290200817664E18 ≈ 2.43 x 1018
      */
-    public double factorial(int number)
-    {
+    public double factorial(int number) {
         double x = number;
         double facTotal = x;
         while (x != 1) {
-            x = x-1;
+            x = x - 1;
             facTotal = facTotal * x;
 
         }
@@ -27,29 +28,58 @@ public class WhileLoopFun
     }
 
     public void printDigits(int number) {
-        String s =String.valueOf(number);
+        String s = String.valueOf(number);
         int x = s.length();
         int y = 0;
         String digits = "";
         while (x != y) {
-            digits = s.substring(x-1,x);
+            digits = s.substring(x - 1, x);
             System.out.println(digits);
             x--;
         }
     }
 
     public int sumOfDigits(int number) {
-        String s =String.valueOf(number);
+        String s = String.valueOf(number);
         int x = s.length();
         int y = 0;
         int sum = 0;
         String digits = "";
         while (x != y) {
-            digits = s.substring(x-1,x);
+            digits = s.substring(x - 1, x);
             int total = Integer.parseInt(digits);
             sum = sum + total;
             x--;
         }
         return sum;
     }
+
+    public boolean isPrime(int number) {
+        int d = 2;
+        boolean prime = true;
+        while (d <= number / 2 ) {
+            if (number % d == 0) {
+                prime =false;
+                d++;
+            } else if (number % d != 0) {
+                prime = true;
+                d++;
+            }
+        }
+        if (number == 1) {
+            prime = false; }
+        return prime;
     }
+
+
+    public int maxDoubles(int number, int threshold) {
+            int d =0;
+            while (number <= threshold) {
+            number = number *2;
+            if (number <= threshold) {
+                d++;
+            }
+        }
+        return d;
+    }
+}
